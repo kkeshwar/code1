768ee51dbcbea188c49c76df7ecb3df955a9f342
kkeshwar <kartik2014.kk@gmail.com>
2018-10-20 11:31:37 +0530

    1st commit

---
+++ Employee.java
@@ -0,0 +1,20 @@
+public class Employee
+{
+	String ename;
+	int ecode;
+	public Employee()
+	{
+	ename="K.Karthikeshwar";
+	ecode=1852;
+	}
+	public void disp()
+	{
+	System.out.println("Employee Name = +ename);
+	System.out.println("Employee Code = "+ecode);
+	}	
+	public static void main(String args[])
+	{
+	Employee emp=new Employee();
+	emp.disp();
+	}
+}
